65e7fab8b065d1a2474c4df5ff29977e7cc9af92
Shizhongqi <62776379+ishizhongqi@users.noreply.github.com>
2026-03-10 15:13:39 +0800

    Include windows.h

--- src/database/driver/odbc_driver.h
+++ src/database/driver/odbc_driver.h
@@ -10,6 +10,16 @@
 #include <string>
 #include <vector>
 
+#if defined(_WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+#endif
+
 #include <sql.h>
 #include <sqlext.h>
 
